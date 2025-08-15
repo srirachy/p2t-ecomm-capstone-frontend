a11e4d5bf50c4fa115e59b1a5b08f254510ba980
srirachy <caseyereyes@gmail.com>
2025-08-15 16:10:25 -0700

    add another strat to fix redirect

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,11 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    build: {
+      outDir: 'dist',
+      assetsInlineLimit: 0,
+    },
+    publicDir: 'public',
     server: {
       proxy: {
         '/payment/stripe-callback': {
