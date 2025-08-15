5e9c541e35b474776ff70be4ae1fb2137296137a
srirachy <caseyereyes@gmail.com>
2025-08-15 14:25:45 -0700

    update vite config with additional env variable

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/payment/stripe-callback': {
-          target: env.VITE_API_BASE_URL || 'http://localhost:5000',
+          target: env.VITE_API_BASE_URL || env.VITE_BACKEND_URI,
           changeOrigin: true,
         }
       }
