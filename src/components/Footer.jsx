c34b6d03cea693081faeebca777ddb7c85f78e3b
srirachy <caseyereyes@gmail.com>
2025-08-15 19:57:17 -0700

    update home, navbar, and footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,18 @@
+import logoImg from '../assets/logo-footer.png';
 import '../styles/Footer.css';
 
 const Footer = () => {
   return (
     <footer>
       <div id='logo-link-container'>
-        <div id='logo-container'>Logo</div>
-        <div id='link-container'>Links</div>
-      </div>
-      <div id='copyright-policy-container'>
-        <div id='copyright-container'>Copyright</div>
-        <div id='privacy-container'>Privacy Policy</div>
+        <figure id='footer-logo-container'>
+          <img src={logoImg} alt={''}/>
+        </figure>
+        <ul id='footer-link-container'>
+          <li>Terms of Use</li>
+          <li>Privacy Notice</li>
+          <li>Legal Info</li>
+        </ul>
       </div>
     </footer>
   )
